2e448b54a29112429f6a690f27c79e8c6012aa75
DEMETRIO DOS SANTOS TRIPODI <132687522+Demetrio023@users.noreply.github.com>
2024-11-12 11:01:41 -0300

    projeto conta banco

--- src/contaTerminal.java
+++ src/contaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Olá Seja Bem Vindo! ao Banco itau");
+         Scanner sc= new Scanner(System.in).useLocale(Locale.US);
+
+         int conta = 0;
+         String  agencia = "";
+         String nomeUsuario = "";
+         double saldo = 237.48;
+
+          
+         System.out.println("digite o numero da sua conta ");
+           conta = sc.nextInt();
+
+         System.out.println("digite o numero da sua agencia ");
+           agencia = sc.next();
+
+         System.out.println("digite o seu nome  ");
+           nomeUsuario = sc.next();
+
+           System.out.println("Olá " + nomeUsuario+" Obrigado por criar uma conta em nosso Banco");
+           System.out.println("sua Agencia é , Agencia: " + agencia);
+           System.out.println("sua Conta é , Conta: " + conta);
+           System.out.println("Seu Saldo é , Saldo: " + saldo);
+           
+
+         
+           
+
+        
+
+
+        
     }
 }
